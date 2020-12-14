769a0ce6ea4a26e27a5a69d4d104ac20ebcb4dc1
behatl <bryce@hashicorp.com>
2020-12-14 13:53:21 -0500

    MSFT CSA 12/14

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   version = "=2.0.0"
   features {}
 }
-#Comment line 
+#Comment line 12/14
 
 resource "azurerm_resource_group" "hashicorp-consul-pov" {
   name     = var.resource_group
