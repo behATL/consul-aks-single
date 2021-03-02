eeada81f018d50ae5576431b071a12ff63fd03e6
behatl <bryce@hashicorp.com>
2021-03-02 14:21:30 -0500

    3/2 CAI

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   version = "=2.0.0"
   features {}
 }
-#Comment line 1/21 a test
+#Comment line 3/2 a test
 
 resource "azurerm_resource_group" "hashicorp-consul-pov" {
   name     = var.resource_group
