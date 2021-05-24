eb3501154e15a33e5b7bf78d3b6e258e1ec9b664
behatl <bryce@hashicorp.com>
2021-05-24 13:36:22 -0400

    Dave test

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   version = "=2.0.0"
   features {}
 }
-#Comment line 5/13 workshop
+#Comment line 5/24 workshop
 
 resource "azurerm_resource_group" "hashicorp-consul-pov" {
   name     = var.resource_group
